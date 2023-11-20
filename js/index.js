878e150a8b43fb93a27cbb2e0aeb11c2da997de0
zederbear <95325867+zederbear@users.noreply.github.com>
2023-11-19 22:00:49 -0700

    button frfr

--- js/index.js
+++ js/index.js
@@ -2,3 +2,12 @@ var data = {
     money: 0,
     upgrades: 1
 }
+
+function moreMoney() {
+    data.money += upgrades;
+    updateScreen();
+}
+
+function updateScreen() {
+    document.getElementById('moneyNum').innerHTML = data.money
+}
